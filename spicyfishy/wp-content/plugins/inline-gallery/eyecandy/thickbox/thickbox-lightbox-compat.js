86fcd8e74689566033f55172463667b66219c605
Chad Jewsbury <chadjewsbury@gmail.com.com>
2014-02-26 11:40:21 -0700

    Removes old plugins and updates wordpress

--- spicyfishy/wp-content/plugins/inline-gallery/eyecandy/thickbox/thickbox-lightbox-compat.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery("a.thickbox").attr("rel", function(){
-		return this.rel.replace(/[\[\]]/g, "_");
-	});
-});
--- spicyfishy/wp-content/plugins/rb-internal-links/templates/tinymce-plugin/search.php
+++
@@ -1,9 +0,0 @@
-<ul class="box-list">
-	<li class="content">
-		<h2><?php _e('Search posts', 'rb-internal-links'); ?></h2>
-		<form id="postSearch" onsubmit="itemClick(__boxID__, this); performSearch(this); return false;">
-			<input type="text" name="q" value="" />
-			<input type="submit" name="submit" value="<?php _e('Search', 'rb-internal-links'); ?>" />
-		</form>
-	</li>
-</ul>

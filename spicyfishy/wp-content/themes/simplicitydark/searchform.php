43c147132bb9cbc8bd927934dc9f76d1d41888a1
Chad Jewsbury <cjewsbury@groupon.com>
2014-04-06 16:52:24 -0500

    Removes old themes

--- spicyfishy/wp-content/themes/simplicitydark/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" class="searchform" action="<?php
- echo $_SERVER['PHP_SELF']; ?>">
-<div>
-<input type="text" value="SUCHBEGRIFF EINGEBEN..." name="s" class="s" onblur="if(this.value=='')this.value='SUCHBEGRIFF EINGEBEN...';" onfocus="if(this.value=='SUCHBEGRIFF EINGEBEN...')this.value='';" />
-</div>
-</form>
